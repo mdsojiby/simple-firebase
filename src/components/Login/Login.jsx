930e007e0ce06c95f47a40b9ea3caebcc6afc630
sojib shikder <mdsojiby2024@gmail.com>
2024-11-10 16:28:18 +0600

    all commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,13 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { auth } from "../firtebase/firebase.init";
-
-
-
-
-
-
-
+import { useState } from "react";
 
 
 const Login = () => {
+
+    const [user,setUser]=useState(null);
  const provider= new GoogleAuthProvider();
+ const githubProvider= new GithubAuthProvider();
 
     
 
@@ -18,17 +15,78 @@ const Login = () => {
         signInWithPopup(auth,provider)
 
         .then(result=>{
-            console.log(result)
+            console.log(result.user)
+            setUser (result.user)
         })
 
         .catch(error=>{
             console.log('Error',error)
+            setUser(null)
         })
+
+       
        
     }
+
+    const handleGithubSignIn=()=>{
+        signInWithPopup(auth,githubProvider)
+        
+        .then(result=>{
+            console.log(result.user)
+            setUser(result.user)
+        })
+
+        .catch(error=> {
+            console.log('error',error)
+        })
+    }
+
+
+    const handleSignOut=()=>{
+
+       signOut(auth)
+
+       .then (()=>{
+        console.log('sign out')
+        setUser(null)
+       })
+
+       .catch(error=>{
+        console.log(error)
+       })
+    }
+
     return (
         <div>
-            <button onClick={handleGoogleSignIn}>Login With Google</button>
+            {/* <button onClick={handleGoogleSignIn}>Login With Google</button>
+             <button onClick={handleSignOut} >Sign out</button> */}
+
+
+            {
+
+                user ?  <button onClick={handleSignOut} >Sign out</button>:
+                <>
+
+
+                <button onClick={handleGoogleSignIn}>Login With Google</button>
+                <button onClick={handleGithubSignIn}>Github Sign  In</button>
+
+                </>
+
+
+            }
+            {
+                user && 
+                
+                <div>
+                    <h4> Name:{user.displayName}</h4>
+                    <p>email:{user.email}</p>
+                    <p>id:{user.providerId}</p>
+
+
+
+                </div>
+            }
         </div>
     );
 };
